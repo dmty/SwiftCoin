8fc5ca4a6bdf274231b391a8fbf56d75fe611366
Dmitry Yako <dmtyak@gmail.com>
2024-07-31 16:37:17 +1200

    Add all coins view

---
+++ SwiftCoin/Core/Home/Views/AllCoinsView.swift
@@ -0,0 +1,41 @@
+//
+//  AllCoinsView.swift
+//  SwiftCoin
+//
+//  Created by Dmitry on 31/07/2024.
+//
+
+import SwiftUI
+
+struct AllCoinsView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("All Coins")
+                .font(.headline)
+                .padding()
+            
+            HStack {
+                Text("Coin")
+                
+                Spacer()
+                
+                Text("Price")
+            }
+            .foregroundColor(.gray)
+            .font(.caption)
+            .padding(.horizontal)
+            
+            ScrollView {
+                VStack {
+                    ForEach(0 ..< 50, id: \.self) { _ in
+                        CoinRowView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AllCoinsView()
+}
---
+++ SwiftCoin/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,61 @@
+//
+//  CoinRowView.swift
+//  SwiftCoin
+//
+//  Created by Dmitry on 31/07/2024.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        HStack {
+            // market cap rank
+            Text("1")
+                .font(.caption)
+                .foregroundColor(.gray)
+            
+            // image
+            Image(systemName: "bitcoinsign.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+            
+            // coin name info
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Bitcoin")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 4)
+                
+                Text("BTC")
+                    .font(.caption)
+                    .padding(.leading, 6)
+            }
+            .padding(.leading, 2)
+            
+            Spacer()
+            
+            // coin price info
+            VStack(alignment: .trailing, spacing: 4) {
+                Text("$60,424.00")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 4)
+                
+                Text("-5.60% ")
+                    .font(.caption)
+                    .padding(.leading, 6)
+                    .foregroundColor(.red)
+            }
+            .padding(.leading, 2)
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
