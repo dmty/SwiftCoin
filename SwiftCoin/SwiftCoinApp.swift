305a3ff2aa97ff7cad80dcfe4800ca6f338e32a4
Dmitry Yako <dmtyak@gmail.com>
2024-07-31 15:54:34 +1200

    Initial Commit

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Dmitry on 31/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
